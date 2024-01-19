hclee/exfat-tools

fsck: repairing File.SecondaryCount using Stream.NameLength
If the count of Name entries is 17, but File.SecondaryCount
is greater than 18, File.SecondaryCount was not fixed.

The count of Name entries is verfied using
Stream.NameLength instead of File.SecondaryCount,
and Vendor Extension and Vendor Allocation entries
are also considered when counting Secondary entries.

Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

include/exfat_ondisk.h
@@ -60,6 +60,8 @@
 #define EXFAT_STREAM		0xC0	/* stream entry */
 #define EXFAT_NAME		0xC1	/* file name entry */
 #define EXFAT_ACL		0xC2	/* stream entry */
+#define EXFAT_VENDOR_EXT	0xD0
+#define EXFAT_VENDOR_ALLOC	0xD1
 
 /* checksum types */
 #define CS_DIR_ENTRY		0

exfatprogs/exfatprogs

include/exfat_ondisk.h
@@ -61,6 +61,8 @@
 #define EXFAT_STREAM		0xC0	/* stream entry */
 #define EXFAT_NAME		0xC1	/* file name entry */
 #define EXFAT_ACL		0xC2	/* stream entry */
+#define EXFAT_VENDOR_EXT	0xE0
+#define EXFAT_VENDOR_ALLOC	0xE1
 
 /* checksum types */
 #define CS_DIR_ENTRY		0